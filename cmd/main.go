bd74485065af49f915fbd6f0f567a827eec4d8d0
glebnaz <nazemnovgleb@gmail.com>
2022-07-21 18:23:45 +0200

    add gh action

---
+++ cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.TextFormatter{
+		DisableColors: false,
+	})
+	log.SetOutput(os.Stdout)
+	level, err := log.ParseLevel(os.Getenv("LOGLVL"))
+	if err != nil {
+		level = log.DebugLevel
+	}
+	log.SetLevel(level)
+}
+
+func main() {
+	log.Infof("Starting...")
+}
